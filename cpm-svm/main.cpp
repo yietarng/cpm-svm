b7ee5792d7edb1521f9f27346f38f7e5fb72cf4e
Samarin.Sergey92@gmail.com <Samarin.Sergey92@gmail.com@1cd552d5-8f93-bf93-fd94-2947633e5915>
2014-05-28 08:15:12 +0000

    Ready version

--- cpm-svm/main.cpp
+++ cpm-svm/main.cpp
@@ -8,72 +8,72 @@ using namespace std;
 
 
 
-#define BMRM_INFO
-
-
-////==============================
-//#include <CGAL/basic.h>
-//#include <CGAL/MP_Float.h>
-//#include <CGAL/Gmpz.h>
-//#include <CGAL/to_rational.h>
-//typedef CGAL::Gmpz ET;
-//typedef CGAL::Quotient<int> Rational;
-////==============================
-
 
 int main(int argc, char* argv[])
 {
-    if(argc!=5)
+    if(argc!=7)
     {
-        cout << "Usage: lsvm <data_filename> <C_value> <epsilon> <max_iter>" << endl;
+        cout << "Usage: lsvm <data_filename> <train_portion> <mix_seed> <C_value> <epsilon> <max_iter>" << endl;
         return 1;
     }
+    const char* dataPathStr = argv[1];
+    const char* trainPortionStr = argv[2];
+    const char* seedStr = argv[3];
+    const char* cValueStr = argv[4];
+    const char* epsilonStr = argv[5];
+    const char* maxIterStr = argv[6];
 
 
-    // "/home/sergei/libsvm_data/test2.data"
 
+    cout << "Data file: " << dataPathStr << endl;
     Data data;
-    data.ReadFile(argv[1]);
+    data.ReadFile(dataPathStr);
     if(!data.IsLoaded())
     {
         cout << "Error while loading data" << endl;
         return 1;
     }
+    cout << "Number of variables: " << data.Samples().size2() << endl;
 
-    Real cValue = Real(atof(argv[2]));
-    Real epsilon = Real(atof(argv[3]));
-    int maxIter = atoi(argv[4]);
 
-    data.SetTrainTestSplit(0.6);
+    float trainPortion = atof(trainPortionStr);
+    data.SetTrainTestSplit(trainPortion);
+    cout << trainPortion*100 << "% of data is used for training" << endl;
     cout << "Train sample count: " << data.TrainSampleIdx().size() << endl;
     cout << "Test sample count: " << data.TestSampleIdx().size() << endl;
-    cout << "Number of variables: " << data.Samples().size2() << endl;
-//    cout << "Train sample indexes: ";
-//    for(int i = 0;i<data.TrainSampleIdx().size();i++)
-//    {
-//        cout << data.TrainSampleIdx()[i] << ' ';
-//    }
-//    cout << endl;
 
-    data.Mix();
-    cout << "Data mixed" << endl;
 
-    cout << "Data file: " << argv[1] << endl;
+    int seed = atoi(seedStr);
+    if(seed>=0)
+    {
+        srand(unsigned(seed));
+        data.Mix();
+        cout << "Data mixed with seed=" << seed << endl;
+    }
+    else
+    {
+        cout << "Data not mixed" << endl;
+    }
+
+
+
+    Real cValue = Real(atof(cValueStr));
+    Real epsilon = Real(atof(epsilonStr));
+    int maxIter = atoi(maxIterStr);
+
+    cout << endl;
     cout << "Cvalue = " << cValue << endl;
     cout << "Epsilon = " << epsilon << endl;
     cout << "Max number of iterations = " << maxIter << endl;
-    cout << "Starting svm..." << endl;
+
+
+    cout << "Svm training..." << endl;
     SVM svm;
     svm.Train(data, cValue, epsilon, maxIter);
+    cout << "Svm training completed." << endl;
+
     cout << "Train error: " << svm.CalcError(data, SVM::TRAIN) << endl;
     cout << "Test error: " << svm.CalcError(data, SVM::TEST) << endl;
-
-//    CGAL::MP_Float fl(334.125);
-//    ET et(fl.);
-//    cout << fl << endl;
-//    cout << et << endl;
-    //cout << CGAL::to_rational<Rational>(334.125) << endl;
-
 
     return 0;
 }
--- cpm-svm/svm.cpp
+++ cpm-svm/svm.cpp
@@ -5,6 +5,11 @@
 #include <iostream>
 
 
+using namespace std;
+
+
+
+
 //=======================================
 // mosek declarations begin
 //=======================================
@@ -22,38 +27,8 @@ static void MSKAPI printstr(void *handle,
 //=======================================
 
 
-using namespace std;
+
 //using namespace boost::numeric::ublas;
-
-
-
-//=============================================
-// CGAL declarations begin
-//=============================================
-
-//#include <cassert>
-//#include <CGAL/to_rational.h>
-//#include <CGAL/basic.h>
-//#include <CGAL/QP_models.h>
-//#include <CGAL/QP_functions.h>
-//// choose exact integral type
-//#ifdef CGAL_USE_GMP
-//#include <CGAL/Gmpz.h>
-//typedef CGAL::Gmpz ET;
-//#else
-//#include <CGAL/MP_Float.h>
-//typedef CGAL::MP_Float ET;
-//#endif
-
-////typedef Real ET;
-
-//// program and solution types
-//typedef CGAL::Quadratic_program<int> Program;
-//typedef CGAL::Quadratic_program_solution<ET> Solution;
-
-//=============================================
-// CGAL declarations end
-//=============================================
 
 
 
@@ -84,8 +59,6 @@ void SVM::Train(const Data& data, const Real cValue, const Real epsilon, const i
     {
         assert(responses[i]==-1 || responses[i]==1);
     }
-//    vector<int> testSampleIdx = data.TestSampleIdx();
-//    vector<int> trainSampleIdx = data.TrainSampleIdx();
 
     int n = data.TrainSampleIdx().size(); // число прецедентов в обучающей выборке
     int d = data.Samples().size2(); // размерность пространства признаков
@@ -103,7 +76,7 @@ void SVM::Train(const Data& data, const Real cValue, const Real epsilon, const i
     Vec w(d);
     std::fill(w.begin(), w.end(), 0);
     int t = 0;
-    int currentEps = 1;
+    Real currentEps = -1;
     vector<Vec> a;
     vector<Real> b;
 
@@ -111,12 +84,6 @@ void SVM::Train(const Data& data, const Real cValue, const Real epsilon, const i
 
     do
     {
-//        if(t%10==0)
-//        {
-//            cout << ".";
-//        }
-
-
         t++;
         a.push_back( empRiskSubgradient(data, w) );
         b.push_back( empRisk(data, w) - inner_prod(w, a.back()) );
@@ -130,68 +97,7 @@ void SVM::Train(const Data& data, const Real cValue, const Real epsilon, const i
         cout << "b[" << t << "] = " << b.back() << endl;
 #endif
 
-//=======================================================
-// SGAL begin
-//=======================================================
 
-//        Program qp(CGAL::EQUAL, true, 0, false, 0);
-
-//        for(int i = 0;i<t;i++)
-//        {
-//            qp.set_a(i, 0, 1);
-//        }
-
-//        qp.set_b(0, 1);
-
-//        for(int i = 0;i<t;i++)
-//        {
-//            for(int j = 0;j<=i;j++)
-//            {
-//                qp.set_d( i, j, inner_prod(a[i], a[j])  );
-//            }
-//        }
-
-//        for(int i = 0;i<t;i++)
-//        {
-//            qp.set_c( i, -lambda*b[i] );
-//        }
-
-
-
-//        // solve the program, using ET as the exact type
-//        Solution s = CGAL::solve_quadratic_program(qp, ET());
-//        assert (s.solves_quadratic_program(qp));
-
-////        for (Solution::Index_iterator it = s.basic_variable_indices_begin();
-////                it != s.basic_variable_indices_end(); ++it)
-////        {
-////            std::cout << *it << " ";
-////        }
-////        cout << endl;
-
-//        Vec alpha(t);
-//        Solution::Variable_numerator_iterator it = s.variable_numerators_begin();
-//        int k = 0;
-//        for(;it!=s.variable_numerators_end();it++)
-//        {
-//            alpha[k] = (*it).to_double()/s.variables_common_denominator().to_double();
-//            k++;
-//        }
-
-
-//        Vec temp(d);
-//        std::fill(temp.begin(), temp.end(), 0);
-//        for(int i = 0;i<t;i++)
-//        {
-//            temp = temp + alpha[i]*a[i];
-//        }
-//        temp = -temp/lambda;
-//        w = temp;
-//        cout << "w[" << t << "] = " << w << endl;
-
-//==================================================
-// CGAL end
-//==================================================
 
         //==========================================
         // argmin begin
@@ -363,28 +269,13 @@ void SVM::Train(const Data& data, const Real cValue, const Real epsilon, const i
     }
     while(currentEps>epsilon && t<tMax);
 
-    cout << endl;
-    cout << "Training completed." << endl;
-    cout << "J(w) = " << Omega(w)+empRisk(data, w) << endl;
-    cout << "Achieved epsilon: " << currentEps << " (required - " << epsilon << ")" << endl;
-    cout << "Number of iterations: " << t << " (max - " << tMax << ")" << endl;
+    cout << "BMRM => J(w) = " << Omega(w)+empRisk(data, w) << endl;
+    printf("BMRM => Achieved epsilon: %e (required - %e)\n", currentEps, epsilon);
+    cout << "BMRM => Number of iterations: " << t << " (max - " << tMax << ")" << endl;
 
 
 
 
-
-//    Vec w;
-//    w.resize(2);
-//    w[0] = 0.8; w[1] = -0.8;
-
-//    cout << "Omega = " << Omega(w) << endl;
-//    cout << "empRisk = " << empRisk(data, w) << endl;
-//    cout << "subGradient = " << empRiskSubgradient(data, w) << endl;
-    //-----------------------------------
-
-
-    // вектор betta должен быть решением задачи оптимизации,
-    // но здесь он назначается равным [1;1;1...1], в целях тестирования
     betta.resize(d);
     for(int i = 0;i<d;i++)
     {
